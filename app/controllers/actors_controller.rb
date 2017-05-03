7718e8798e81e2b821d3ee06bd6e135f7c716308
Rodman Aguirre Méndez <rodfagui@gmail.com>
2017-05-03 16:30:36 -0500

    Se agrega la funcionalidad para crear un actor

--- app/controllers/actors_controller.rb
+++ app/controllers/actors_controller.rb
@@ -2,4 +2,22 @@ class ActorsController < ApplicationController
 	def index
 	  @actors = Actor.all
 	end
+
+	def new
+    @actor = Actor.new
+  end
+
+  def create
+    @actor = Actor.new(actor_params)
+    if @actor.save
+      redirect_to actors_path, notice: "Se agrego nuevo actor"
+    else
+      render :new
+    end
+  end
+
+  private
+    def actor_params
+      params.require(:actor).permit(:name, :image_url, :bio, :birth_date, :birth_place, :alive, :death_date, :death_place)
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
   get 'actors', to: 'actors#index'
+  get '/actors/new', to: 'actors#new', as: 'new_actor'
+  post "actors", to: 'actors#create'
 end
